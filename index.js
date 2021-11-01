46935a1bae12c824f16952dbc8989dfb7a454f5c
Grotius Hasiholan <grotius.hasiholan@gmail.com>
2021-11-01 23:41:25 +0700

    Adding login and register page

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 9000
+
+app.set('view engine', 'ejs')
+app.use(express.json())
+app.use(express.urlencoded({ extended: false}))
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',(req, res) => {
+    res.render('pages/index')
+})
+
+const game = require('./routers/game')
+app.use(game)
+
+
+app.listen(port, () => {
+    console.log(__dirname)
+    console.log(`Server berjalan di http://localhost:${port}`)
+})
---
+++ routers/game.js
@@ -0,0 +1,113 @@
+var fs = require('fs')
+const express = require('express')
+const router = express.Router()
+let accounts = require('../db/accounts.json')
+
+const def_account = {
+    id: undefined,
+    username: 'PlayerUsername',
+    password: 'PlayerPassword'
+}
+let user = def_account
+
+let login = {
+    status: '',
+}
+
+router.get('/game/login', (req, res) => {
+    res.render('game/login',login)
+})
+
+router.get('/game/logout', (req, res) => {
+    user = def_account,
+    login.status = ''
+    res.redirect('/game/login')
+})
+
+
+//Register account
+router.get('/game/register', (req, res) => {
+    // res.send('Halaman register')
+    res.render('game/register')
+})
+
+router.post('/game/register', (req, res) => {
+    var input = {
+        username: req.body.username,
+        password: req.body.password
+    }
+    //Checking data
+    var result = accounts.filter(account => account.username === input.username)
+    if (result.length > 0) {
+        res.render('game/register')
+    } else {
+        var temp = {
+            id: accounts.length + 1,
+            username: input.username,
+            password: input.password
+        }
+
+        //Push to JSON
+        fs.readFile('db/accounts.json', 'utf-8', (err, data) => {
+            if (err) {
+                console.log(err)
+            }
+            var content = JSON.parse(data)
+            content.push(temp)
+            accounts = content
+            var json = JSON.stringify(content, undefined, 1)
+            fs.writeFile('db/accounts.json', json, 'utf8', (err, data) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    console.log('Register succesfully')
+                }
+            });
+        })
+        login.status = 'Register successfully!'
+        res.redirect('/game/login')
+    }
+})
+
+//Login
+router.post('/game', (req, res) => {
+    var input = {
+        username: req.body.username,
+        password: req.body.password
+    }
+    var result = accounts.filter(account => ((account.username === input.username) && (account.password === input.password)))
+    if (result.length > 0) {
+        user.id = result[0].id
+        user.username = result[0].username
+        console.log(accounts)
+        res.redirect('/game')
+    } else {
+        console.log('Login failed!! :(')
+        res.redirect('/game/login')
+    }
+})
+
+//Middleware
+router.use((req, res, next, ) => {
+    if (user.id) {
+        console.log(user)
+        next()
+    } else {
+        res.redirect('/game/login')
+        next()
+    }
+})
+
+router.get('/game', (req, res) => {
+    res.render('game/game', user)
+})
+
+
+
+router.get('/game/accounts', (req, res) => {
+    res.status(200).json(accounts)
+})
+
+
+
+module.exports = router
