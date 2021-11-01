b5f612b032a0a8228f542835be76e063e094cb16
Grotius Hasiholan <grotius.hasiholan@gmail.com>
2021-11-01 20:06:48 +0700

    Halaman login

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const app = express()
-let users = require('./db/users.json')
 const port = 9000
 
+app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(express.urlencoded({ extended: false}))
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/',(req, res) => {
-    res.send("Hello World")
+    res.render('pages/index')
 })
 
 const game = require('./routers/game')
@@ -15,5 +17,6 @@ app.use(game)
 
 
 app.listen(port, () => {
+    console.log(__dirname)
     console.log(`Server berjalan di http://localhost:${port}`)
 })
--- routers/game.js
+++ routers/game.js
@@ -1,10 +1,58 @@
 const express = require('express')
 const router = express.Router()
+const accounts = require('../db/accounts.json')
+const def_account = {
+    id: undefined,
+    username: 'PlayerUsername',
+    password: 'PlayerPassword'
+}
+let user = def_account
+
+router.get('/game/login', (req, res) => {
+    res.render('game/login')
+})
+
+router.get('/game/register', (req, res) => {
+    res.send('Halaman register')
+})
+
+router.post('/game', (req, res) => {
+    var input = {
+        username: req.body.username,
+        password: req.body.password
+    }
+    var result = accounts.filter(account => ((account.username === input.username) && (account.password === input.password)))
+    if(result.length>0){
+        user.id = result[0].id
+        console.log('Login success!!! :D')
+        res.redirect('/game')
+    } else{
+        console.log('Login fail!! :(')
+        res.redirect('/game/login')
+    }
+})
 
 //Middleware
+router.use((req, res, next, ) => {
+    if (user.id) {
+        console.log(user)
+        next()
+    } else {
+        res.redirect('/game/login')
+        next()
+    }
+})
 
 router.get('/game', (req, res) => {
-    res.send('Ini halaman game')
+    res.render('game/game', user)
 })
+
+
+
+router.get('/game/accounts', (req, res) => {
+    res.status(200).json(accounts)
+})
+
+
 
 module.exports = router
